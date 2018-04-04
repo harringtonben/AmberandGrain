2a4a45dddd46b94cfd5cf15f80658e6d34f0b684
Ben Harrington <harringtonben@icloud.com>
2018-04-03 19:52:10 -0500

    finished createrecipe

--- AmberAndGrain/Controllers/RecipesController.cs
+++ AmberAndGrain/Controllers/RecipesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AmberAndGrain.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +8,20 @@ using System.Web.Http;
 
 namespace AmberAndGrain.Controllers
 {
+    [RoutePrefix("api/recipes")]
     public class RecipesController : ApiController
     {
+        [HttpPost, Route("")]
+        public HttpResponseMessage AddRecipe(RecipesDto recipe)
+        {
+            var recipeRepository = new RecipeRepository();
+            var createRecipe = recipeRepository.Create(recipe);
+
+            if (createRecipe)
+                return Request.CreateResponse(HttpStatusCode.Created);
+
+            return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Sorry we could not create a recipe at this time, please try again later.");
+        }
+
     }
 }
---
+++ AmberAndGrain/Services/RecipeRepository.cs
@@ -0,0 +1,39 @@
+﻿using AmberAndGrain.Controllers;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AmberAndGrain.Services
+{
+    public class RecipeRepository
+    {
+        public bool Create(RecipesDto recipe)
+        {
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["Main"].ConnectionString))
+            {
+                db.Open();
+
+                var createRecipe = db.Execute(@"INSERT INTO Recipes
+                                                            (Name
+                                                            ,PercentWheat
+                                                            ,PercentCorn
+                                                            ,BarrelAge
+                                                            ,BarrelMaterial
+                                                            ,Creator)
+                                                        VALUES
+                                                            (@Name
+                                                            ,@PercentWheat
+                                                            ,@PercentCorn
+                                                            ,@BarrelAge
+                                                            ,@BarrelMaterial
+                                                            ,@Creator)", recipe);
+
+                return createRecipe == 1;
+            }
+        }
+    }
+}
