9134497ba08316100a74e0e94dec0861a43cbc28
Ben Harrington <harringtonben@icloud.com>
2018-04-03 21:09:52 -0500

    finished create batch method

---
+++ AmberAndGrain/Services/BatchRepository.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AmberAndGrain.Services
+{
+    public class BatchRepository
+    {
+        public bool Create(int recipeId, string cooker)
+        {
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["Main"].ConnectionString))
+            {
+                db.Open();
+
+                var createBatch = db.Execute(@"INSERT INTO Batches
+                                                            (RecipeId
+                                                            ,Cooker
+                                                            )
+                                                        VALUES
+                                                            (@recipeId
+                                                            ,@cooker
+                                                            )", new { recipeId, cooker});
+
+                return createBatch == 1;
+            }
+        }
+    }
+}
