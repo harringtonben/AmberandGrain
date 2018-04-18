667d3158f55f29478566bfb64d004d842870fe56
Ben Harrington <harringtonben@icloud.com>
2018-04-17 20:43:30 -0500

    finished create edit and read of recipes

---
+++ AmberAndGrain/Controllers/RecipeController.cs
@@ -0,0 +1,61 @@
+﻿using AmberAndGrain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AmberAndGrain.Controllers
+{
+    public class RecipeController : Controller
+    {
+        // GET: Recipe
+        public ActionResult Index()
+        {
+            var recipeRepository = new RecipeRepository();
+            var recipes = recipeRepository.GetAll();
+
+            return View(recipes);
+        }
+
+        //Get: Recipe/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Post: Recipe/Create
+        [HttpPost]
+        public ActionResult Create(RecipesDto recipe)
+        {
+            //save it to the database
+            var recipeRepository = new RecipeRepository();
+            var createNew = recipeRepository.Create(recipe);
+
+            return RedirectToAction("Index");
+        }
+
+        //Get: Recipe/Edit/id
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var repo = new RecipeRepository();
+            var recipe = repo.Get(id);
+
+            if (recipe == null)
+                return HttpNotFound("The recipe you requested does not exist");
+
+            return View(recipe);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, RecipesDto recipe)
+        {
+            var repo = new RecipeRepository();
+            repo.Update(recipe, id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- AmberAndGrain/Global.asax.cs
+++ AmberAndGrain/Global.asax.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace AmberAndGrain
@@ -12,6 +14,10 @@ namespace AmberAndGrain
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
